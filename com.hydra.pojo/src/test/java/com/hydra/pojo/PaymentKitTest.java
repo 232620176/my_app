9176e9aa9981ba0c4b26bcb74152495d7c27a1cd
王爽 <wangshuang@lakala.com>
2017-01-22 13:47:35 +0800

    修正

--- com.hydra.pojo/src/test/java/com/hydra/pojo/PaymentKitTest.java
+++ com.hydra.pojo/src/test/java/com/hydra/pojo/PaymentKitTest.java
@@ -1,11 +1,19 @@
 package com.hydra.pojo;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class PaymentKitTest{
 	@Test
 	public void testSayHello(){
-		
+		Map<String, Object> paymentType = new HashMap<String, Object>();
+		paymentType.put("PaymentType", "G");
+		PaymentKit pk = new PaymentKit.BuildTool().buildGPayment(paymentType).build();
+		assertEquals(true, pk.hasG);
+		assertEquals(false, pk.hasW);
 	}
 }
--- com.hydra.util/src/test/java/com/hydra/util/MapUtilTest.java
+++ com.hydra.util/src/test/java/com/hydra/util/MapUtilTest.java
@@ -1,6 +1,5 @@
 package com.hydra.util;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MapUtilTest{
