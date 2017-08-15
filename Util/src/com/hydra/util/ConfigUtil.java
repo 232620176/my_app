8b9b663c817936bc1795edfce0a6149b85807fa5
wangshuang <232620176@qq.com>
2017-08-15 21:54:27 +0800

    配置加载

---
+++ Util/src/com/hydra/util/ConfigUtil.java
@@ -0,0 +1,32 @@
+package com.hydra.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigUtil {
+	private ConfigUtil(){throw new UnsupportedOperationException();}
+	private static final Properties PRO = new Properties();
+	
+	public static String get(String key){
+		return PRO.getProperty(key);
+	}
+	
+	public static void load(String path) throws IOException{
+		synchronized(PRO){
+			PRO.clear();
+			PRO.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(path));
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(get("db.username"));
+	}
+	
+	static{
+		try {
+			load("conf/dev.properties");
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e.getMessage());
+		}
+	}
+}
