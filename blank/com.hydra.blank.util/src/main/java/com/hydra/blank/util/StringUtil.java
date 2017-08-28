fcd4ebd1c1429acef329616f9c6f7d2186af9576
wangshuang <232620176@qq.com>
2017-08-28 23:06:47 +0800

    init

---
+++ blank/com.hydra.blank.util/src/main/java/com/hydra/blank/util/StringUtil.java
@@ -0,0 +1,12 @@
+package com.hydra.blank.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StringUtil {
+	private static Logger logger = LoggerFactory.getLogger(StringUtil.class);
+	
+	public static void main(String[] args) {
+		logger.info("Hello World!");
+	}
+}
