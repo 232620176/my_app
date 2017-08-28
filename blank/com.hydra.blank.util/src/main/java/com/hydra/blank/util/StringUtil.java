ec2a7b7c527ac762dfe7ff21b2e17dd84bacfc24
wangshuang <232620176@qq.com>
2017-08-28 23:12:42 +0800

    add utils

--- blank/com.hydra.blank.util/src/main/java/com/hydra/blank/util/StringUtil.java
+++ blank/com.hydra.blank.util/src/main/java/com/hydra/blank/util/StringUtil.java
@@ -4,7 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StringUtil {
+	private StringUtil(){throw new UnsupportedOperationException();}
 	private static Logger logger = LoggerFactory.getLogger(StringUtil.class);
+	
+	public static boolean isEmpty(Object target){
+		return null == target || "".equals(target);
+	}
 	
 	public static void main(String[] args) {
 		logger.info("Hello World!");
