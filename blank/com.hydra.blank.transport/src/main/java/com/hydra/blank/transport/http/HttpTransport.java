0c3421fd7ae15ec32ffc08fef5bd2ff2c0eb727c
wangshuang <wangshuang@lakala.com>
2017-09-04 17:21:31 +0800

    division http transport

--- blank/com.hydra.blank.transport/src/main/java/com/hydra/blank/transport/http/HttpTransport.java
+++ blank/com.hydra.blank.transport/src/main/java/com/hydra/blank/transport/http/HttpTransport.java
@@ -68,19 +68,6 @@ public class HttpTransport extends AbstractHttpTransport {
 		}
 	}
 	
-	public static void main(String[] args) throws Exception{
-		ApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"classpath*:spring/transport/transport.xml"});
-		HttpTransport httpClient = CommonUtil.transfer(context.getBean("httpClient"));
-		Map<String, Object> param = MapUtil.getMap();
-		param.put(Dict._SUBCHANNELID, "100017");
-		param.put("wxInx", 3);
-		String url = "http://10.5.31.13:9060/adaptor/convert.do?_t=json&_TransCode=getWxAccessToken&wxInx=2";
-		String res = httpClient.doSubmit(url, param, new HttpTransport.SimpleGetMehtodProcessor());
-		param = HttpResultProcessor.processResult(res);
-		System.out.println(param);
-		((ClassPathXmlApplicationContext)context).close();
-	}
-	
 	/**
 	 * PostMethod简单处理器，参数形式：key-value
 	 * <p>
