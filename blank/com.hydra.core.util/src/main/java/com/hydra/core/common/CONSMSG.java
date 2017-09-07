751b8396581860768acdaef1dcd17a734501ea54
wangshuang <wangshuang@lakala.com>
2017-09-07 18:42:28 +0800

    常量

---
+++ blank/com.hydra.core.util/src/main/java/com/hydra/core/common/CONSMSG.java
@@ -0,0 +1,10 @@
+package com.hydra.core.common;
+
+public class CONSMSG {
+	public static final String VALUE_0 = "0";// 1
+	public static final String VALUE_1 = "1";// 1
+	public static final String VALUE_2 = "2";// 1
+	
+	// 静态工具类，防误生成
+	private CONSMSG(){throw new UnsupportedOperationException();}
+}
