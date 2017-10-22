5542bbc254e7ec1158309b68dc8767eb3a035d29
wangshuang <232620176@qq.com>
2017-10-22 15:38:18 +0800

    模板化运行

--- blank/com.hydra.hadoop/src/main/java/com/hydra/hadoop/mr/wc/JobRun.java
+++ blank/com.hydra.hadoop/src/main/java/com/hydra/hadoop/mr/wc/JobRun.java
@@ -1,30 +1,30 @@
 package com.hydra.hadoop.mr.wc;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.hydra.hadoop.mr.util.MRUtil;
+import com.hydra.hadoop.mr.util.MRUtil.Callable;
 
 public class JobRun {
 	public static void main(String[] args) throws Exception{
-		Configuration conf = new Configuration();
-		
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(JobRun.class);
-		job.setMapperClass(WcMapper.class);
-		job.setReducerClass(WcReducer.class);
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
-		
-//		job.setNumReduceTasks(1);//设置reduce任务的个数
-		
-		//mapreduce 输入数据所在目录或者文件
-		FileInputFormat.addInputPath(job, new Path("/usr/input/wc"));
-		//mr执行之后的输出数据的目录
-		FileOutputFormat.setOutputPath(job, new Path("/usr/output/wc"));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		String tkName = "word count";
+		String tkPath = "wc";
+		String[] path = {MRUtil.BASE_INPUT_PATH + tkPath,
+				MRUtil.BASE_OUTPUT_PATH + tkPath};
+		String[] param = MRUtil.mergeArray(args, path);
+		MRUtil.runTemplate(tkName, new Callable(){
+			@Override
+			public void initJob(Job job) {
+				job.setJarByClass(JobRun.class);
+				
+				job.setMapperClass(WcMapper.class);
+				job.setReducerClass(WcReducer.class);
+				
+				job.setMapOutputKeyClass(Text.class);
+				job.setMapOutputValueClass(IntWritable.class);
+			}
+		}, param);
 	}
 }
