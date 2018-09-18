0338e9e746260dd5bbd0b0b84e38b7bd6e331509
wangshuang <232620176@qq.com>
2018-09-18 18:16:09 +0800

    优化

--- blank/com.hydra.core.util/src/main/java/com/hydra/core/util/RandomUtil.java
+++ blank/com.hydra.core.util/src/main/java/com/hydra/core/util/RandomUtil.java
@@ -1,8 +1,11 @@
 package com.hydra.core.util;
 
 import java.util.Calendar;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import com.hydra.core.common.Dict;
 
 /**
  * 用于随机服务的工具
@@ -56,6 +59,32 @@ public final class RandomUtil {
         return String.format("%0" + size + "d", val);
     }
 
+    /**
+     * <p>
+     * Title: createRandom
+     * </p>
+     * <p>
+     * Description: 获取指定长度的随机字符串，字符串取值范围：HEXADECIMAL_CHAR_UPPER
+     * </p>
+     * 
+     * @param length 指定长度
+     * @return String 随机字符串
+     */
+    public static String randomHexadecimal(int length) {
+        return randomStr(length, Dict.HEXADECIMAL_CHAR_UPPER);
+    }
+
+    public static String randomStr(int length, char[] source) {
+        StringBuilder result = new StringBuilder();
+        int len = source.length;
+        for (int i = 0; i < length; i++) {
+            int itmp = RANDOM.nextInt(len);
+            char ctmp = source[itmp];
+            result.append(ctmp);
+        }
+        return result.toString();
+    }
+
     public static int getLength(int num) {
         return String.valueOf(num).length();
     }
@@ -64,13 +93,19 @@ public final class RandomUtil {
         if (size < 0 || size > 10) {
             size = 10;
         }
-        return (int) (Math.pow(10, size) - 1);
+        return SIZE_MAX.get(size);
     }
 
     public static long getLongMax(int size) {
         return (long) (Math.pow(10, size) - 1);
     }
 
+    private static final Map<Integer, Integer> SIZE_MAX = MapUtil.getHashMap();
+    static {
+        for (int i = 0; i < 11; i++) {
+            SIZE_MAX.put(i, (int) (Math.pow(10, i) - 1));
+        }
+    }
     private static final Integer AGE_MIN  = 16;
     private static final Integer AGE_STEP = 40;
     private static final Integer NOW_YEAR = Calendar.getInstance().get(Calendar.YEAR);
--- blank/com.hydra.core.util/src/main/java/com/hydra/core/util/StringUtil.java
+++ blank/com.hydra.core.util/src/main/java/com/hydra/core/util/StringUtil.java
@@ -1,11 +1,8 @@
 package com.hydra.core.util;
 
-import java.util.Random;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.hydra.core.common.Dict;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,27 +26,21 @@ public final class StringUtil {
         return match.matches();
     }
 
-    /**
-     * <p>
-     * Title: createRandom
-     * </p>
-     * <p>
-     * Description: 获取指定长度的随机字符串，字符串取值范围：HEXADECIMAL_CHAR_UPPER
-     * </p>
-     * 
-     * @param length 指定长度
-     * @return String 随机字符串
-     */
-    public static String createRandom(int length) {
-        Random random = RandomUtil.get();
-        StringBuilder result = new StringBuilder();
-        int len = Dict.HEXADECIMAL_CHAR_UPPER.length - 1;
-        for (int i = 0; i < length; i++) {
-            int itmp = random.nextInt(len);
-            char ctmp = Dict.HEXADECIMAL_CHAR_UPPER[itmp];
-            result.append(ctmp);
+    public static String formatNumber(int num, int size) {
+        String src = String.valueOf(num);
+        int len = src.length();
+        if (size > len) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = size; i > len; i--) {
+                sb.append("0");
+            }
+            sb.append(num);
+            return sb.toString();
+        } else if (size == len) {
+            return src;
+        } else {
+            return src.substring(len - size);
         }
-        return result.toString();
     }
 
     /**
